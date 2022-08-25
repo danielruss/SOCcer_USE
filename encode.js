02287821f2ef308443d1cd7484aa7b1cb7af7ffd
danielruss <druss@mail.nih.gov>
2022-08-25 11:28:29 -0400

    encode uploaded? file

--- encode.js
+++ encode.js
@@ -13,3 +13,35 @@ export async function encode(model, txt, output_element) {
     );
 
 }
+
+export async function encodeFile(model, file_element, output_element) {
+    const selectedFile = file_element.files[0];
+    output_element.innerHTML = `encoding ${selectedFile.name}`
+    Papa.parse(selectedFile, {
+        header: true,
+        before: function (file, inputElem) {
+            console.log(`starting ${file.name} at ${new Date()} `)
+        },
+        complete: async function (results, file) {
+            console.log(`finished ${file.name} at ${new Date()} `)
+            console.log(results)
+
+
+            for (let indx = 1; indx <= 10; indx++) {
+                let txt = results.data[indx].JobTitle
+                let tokens = await model.tokenizer.encode(txt)
+                let embeddings = await model.embed(txt)
+                console.log(embeddings)
+                output_element.insertAdjacentHTML('beforeend',
+                    `<div class="result">
+                        <div class="title">text</div>
+                        <div>${txt}</div>
+                        <div class="title">tokens</div>
+                        <div>${tokens.map(i => model.tokenizer.vocabulary[i][0]).join(", ")}</div>
+                        <div class="title">embeddings</div>
+                        <div>${embeddings}</div>
+                    </div>`)
+            }
+        }
+    })
+}
